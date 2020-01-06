37da54c65b800d8fa0fbc95acc3b1a8230121235
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-06 15:15:33 -0800

    chagne the main npm with electron to make a pdf

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 const inquirer = require("inquirer");
 const axios = require("axios");
-var pdf = require("html-pdf")
+// var pdf = require("html-pdf")
+
 var generateHtml = require("./generateHTML.js")
 const fs = require("fs");
+var convertFactory = require('electron-html-to');
+var conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+  });
+   
 const util = require("util");
-var options = { format:"A4"}
+// var options = { format:"US Letter"}
 const writeToFileAsync = util.promisify(fs.writeFile)
 function promptUser(){
         return inquirer.prompt([
@@ -26,14 +32,24 @@ promptUser().then(function({username,color}){
     axios
     .get(`https://api.github.com/users/${username}`)
     .then(function(res){
-
+        //make if statement if status code is 404
         let data = res.data
         data.color = color
-        writeToFile(generateHtml.generateHTML(data))
-        pdf.create(generateHtml.generateHTML(data),options).toFile("./index.pdf",function(err,res){
-            if(err) return console.log(err)
-            console.log(res)
-        })
+        writeToFile(generateHtml.generateHTML(data));
+        // pdf.create(generateHtml.generateHTML(data),options).toFile("./index.pdf",function(err,res){
+        //     if(err) return console.log(err)
+        //     console.log("Here's your pdf:"+JSON.stringify(res))
+        //     console.log("Here's your html: portfolio.html")
+        // }) 
+        conversion({ html:generateHtml.generateHTML(data)} , function(err, result) {
+            if (err) {
+              return console.error(err);
+            }
+           
+            result.stream.pipe(fs.createWriteStream('./portfolio.pdf',{mode:"0766",flag:"w"}));
+            conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+            
+          });
         
    })
 })
